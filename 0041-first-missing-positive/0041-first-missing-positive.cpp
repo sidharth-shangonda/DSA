bee6c9a21a994d63ffc806423851d8669781663e
Sidharth Shangonda <sidharthshangonda@gmail.com>
2026-07-12 11:26:42 +0530

    Time: 0 ms (100%), Space: 55.2 MB (31.85%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
+    //for this use cyclic sort
+    void swap(int &a,int &b) {
+        int temp=a;
+        a=b;
+        b=temp;
+        return;
+    }
     int firstMissingPositive(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(),nums.end());
-        int i=1;
-        while(s.count(i)) i++;
-        return i;
+        int n=nums.size();
+        for(int i=0;i<n;i++) {
+            //use while to prevent all the numbers 
+            while(nums[i] >= 1 && nums[i] <= n && nums[nums[i]-1] !=nums[i]) swap(nums[i],nums[nums[i] - 1]);
+        }
+        for(int i=0;i<n;i++) {
+            if(nums[i] != i+1) return i+1;
+        }
+        return n+1;
     }
 };
