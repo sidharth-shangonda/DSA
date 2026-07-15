458c67d191f19bf58e80dec7634716acd40bb464
Sidharth Shangonda <sidharthshangonda@gmail.com>
2026-07-15 22:41:36 +0530

    Time: 0 ms (100%), Space: 10.4 MB (72.87%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canShip(vector<int>& weights, int days,int weight) {
+        int d=0;
+        int sum=0;
+        for(int i=0;i<weights.size();i++) {
+            if(sum + weights[i] > weight) {
+                sum=0;
+                d++;
+            }
+            sum+=weights[i];
+        }
+        if(sum>0) d++;
+        return d<=days;
+    }
+    int splitArray(vector<int>& weights, int days) {
+        //no need to sort 
+        int low=*max_element(weights.begin(),weights.end());
+        long long high=accumulate(weights.begin(),weights.end(),0LL);
+        int ans=low-1;
+        //for jump 
+        while (low <= high) {
+            // Find the middle capacity to test
+            long long mid = low + (high - low) / 2; 
+            
+            if (canShip(weights, days, mid)) {
+                // If this capacity works, record it as a potential answer
+                ans = mid; 
+                // But let's see if we can find an even SMALLER capacity that works
+                high = mid - 1; 
+            } else {
+                // If it fails, the capacity is too small. We MUST increase it.
+                low = mid + 1; 
+            }
+        }
+        
+        return ans;
+    }
+};
