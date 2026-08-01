e0075c05481ec85879a160207a1b71a80a351b92
Sidharth Shangonda <sidharthshangonda@gmail.com>
2026-08-01 18:01:13 +0530

    Time: 3 ms (94.36%), Space: 52.1 MB (83.2%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -3,24 +3,30 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         return safe(graph);
     }
-    bool isCyclic(int st,vector<int> &visited,vector<vector<int>>& graph) {
+    bool isCyclic(int st,vector<int> &visited,vector<vector<int>>& graph,vector<int> &result) {
         visited[st]=1;
         for(auto it:graph[st]) {
             if(visited[it]==-1) {
-                if(isCyclic(it,visited,graph)) return true;
+                if(isCyclic(it,visited,graph,result)) return true;
             } else if(visited[it]) {
                 return true;
             }
         }
         visited[st]=0;
+        result.push_back(st);
         return false;
     }
     vector<int> safe(vector<vector<int>>& graph) {
         vector<int> ans;
+        vector<int> visited(graph.size(),-1);
         for(int i=0;i<graph.size();i++) {
-            vector<int> visited(graph.size(),-1);
-            if(!isCyclic(i,visited,graph)) ans.push_back(i);
+            if(visited[i]==-1) {
+                if(!isCyclic(i,visited,graph,ans)) {
+                    continue;
+                }
+            }
         }
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
