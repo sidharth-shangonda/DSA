207395bd7ba46f02c975ce4b75e0ca3c21bf6aaf
Sidharth Shangonda <sidharthshangonda@gmail.com>
2026-08-04 20:29:01 +0530

    Time: 38 ms (37.34%), Space: 19.5 MB (69.61%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,42 +1,36 @@
-class Graph{
+class DisjointSet{
 public:
-    int V;
-    list<int> *adj;
-    Graph(int V) {
-        this->V=V;
-        adj=new list<int>[V];
-    }
-    void addEdge(int u,int v) {
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    void dfsHelper(int st,vector<bool> &visited,int &nodes) {
-        nodes++;
-        visited[st]=true;
-        for(auto it:adj[st]) {
-            if(!visited[it]) {
-                dfsHelper(it,visited,nodes);
-            }
+    vector<int> parent,rank;
+    DisjointSet(int n) {
+        rank.resize(n,0);
+        parent.resize(n);
+        for(int i=0;i<n;i++) {
+            parent[i]=i;
         }
     }
-    int dfs() {
-        vector<bool> visited(V,false);
-        int count=0;
-        for(int i=0;i<V;i++) {
-            if(!visited[i]) {
-                int nodes=0;
-                dfsHelper(i,visited,nodes);
-                count+=nodes-1;
-            }
+    int find(int x) {
+        if(parent[x]==x) return x;
+        return parent[x]=find(parent[x]);//path compression
+    }
+    void unionByRank(int u,int v) {
+        int parA=find(u);
+        int parB=find(v);
+        if(parA ==parB) return;
+        if(rank[parA] < rank[parB]) {
+            parent[parA]=parB;
+        } else if(rank[parA] > rank[parB]) {
+            parent[parB]=parA;
+        } else {
+            parent[parB]=parA;
+            rank[parA]++;
         }
-        return count;
-    } 
+    }
 };
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n=stones.size();
-        Graph graph(n);
+        DisjointSet sets(n);
         for(int i=0;i<n-1;i++) {
             int x=stones[i][0];
             int y=stones[i][1];
@@ -44,11 +38,14 @@ public:
                 int dx=stones[j][0];
                 int dy=stones[j][1];
                 if(x==dx || y == dy) {
-                    graph.addEdge(i,j);
+                    sets.unionByRank(i,j);
                 }
             }
         }
-        int total=graph.dfs();
-        return total;
+        set <int> unique;
+        for(int i=0;i<n;i++) {
+            if(!unique.count(sets.find(i))) unique.insert(sets.find(i));//dont implemet the i insert the parent sets.find(i)
+        }
+        return n-unique.size();//return the n - total subgraphs
     }
 };
