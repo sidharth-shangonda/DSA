373d6ef1659b25239682711893b77436ecf9c4af
Sidharth Shangonda <sidharthshangonda@gmail.com>
2026-09-06 21:33:54 +0530

    Time: 27 ms (71.17%), Space: 27 MB (64.23%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -11,9 +11,9 @@ public:
         for(int i=1;i<=m;i++) {
             for(int j=1;j<=n;j++) {
                 if(s[i-1]==t[j-1]) {
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];//just take the previous sum 
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];//just take the previous sum and the posiible ones 
                 } else {
-                    dp[i][j]=dp[i-1][j];//
+                    dp[i][j]=dp[i-1][j];// take the previous ones 
                 }
             }
         }
